f434008ddb5921a78e3f0da454a95871f0dbdf4b
yikyung <uplift.0508@gmail.com>
2021-12-07 12:58:24 -0900

    Add MainClass for Autocongifuration and Component scanning
    add TriggerClass for run Application

---
+++ src/test/java/com/yigyung/cashoptimizer/CashOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yigyung.cashoptimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
